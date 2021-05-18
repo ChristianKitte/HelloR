372e36e90844c30f1bf65c39288ec047165b54db
Christian Kitte <chkitte@web.de>
2021-05-18 20:03:43 +0200

    Letzte Korrekturen

--- esoph.R
+++ esoph.R
@@ -13,89 +13,92 @@
 #
 # Zum einen geschieht dies durch die Analyse der Daten selbst, aber zum
 # anderen mit dem Wissen um die Herkunft der Daten.
-help(esoph)
 
-writeLines("")
+# In diesem Fall können mit Hilfe der help-Funktion Informationen über das
+# DataSet abgerufen werden.
+help( esoph )
 
-writeLines("Infos aus dem Dokumenationsfenster:")
-writeLines("agegp Altersgruppe  ==> gruppiert")
-writeLines("alcgp Alkoholkonsum ==> gruppiert")
-writeLines("tobgp Tabakkonsum ==> gruppiert")
-writeLines("ncases Anzahl Fälle ==> Anzahl")
-writeLines("ncontrols Anzahl der Kontrollen ==> Anzahl")
+writeLines( "" )
 
-writeLines("")
+writeLines( "Infos aus dem Dokumenationsfenster:" )
+writeLines( "agegp Altersgruppe  ==> gruppiert" )
+writeLines( "alcgp Alkoholkonsum ==> gruppiert" )
+writeLines( "tobgp Tabakkonsum ==> gruppiert" )
+writeLines( "ncases Anzahl Fälle ==> Anzahl" )
+writeLines( "ncontrols Anzahl der Kontrollen ==> Anzahl" )
+
+writeLines( "" )
 
 # Verfügbar machen des gesuchten Datasets
 esoDS <- datasets::esoph
 
 # Mit attributes erhalten wir einen Überblick über die vorhandenen Spalten
 # und den Datentyp des objektes
-dsAttribs <- attributes(esoDS)
-writeLines("Eigenschaften:")
+dsAttribs <- attributes( esoDS )
+writeLines( "Eigenschaften:" )
 dsAttribs
 
-writeLines("")
+writeLines( "" )
 
 # Mit nrow und ncol erhalten wir einen weiteren Einblick in den Umfang der
 # vorliegenden Daten
-dsRows <- nrow(esoDS)
-writeLines("Reihen:")
+dsRows <- nrow( esoDS )
+writeLines( "Reihen:" )
 dsRows
 
-writeLines("")
+writeLines( "" )
 
-dsCols <- ncol(esoDS)
-writeLines("Spalten:")
+dsCols <- ncol( esoDS )
+writeLines( "Spalten:" )
 dsCols
 
-writeLines("")
+writeLines( "" )
 
 # Die Ausgabe der ersten und letzten Datensätze gibt einen weitern Überblick
 # über die zu erwartende Qualität. Die Qualität kann trotzdessen in der Mittes
-# des Datensatzen nochmals komplett anders sein
-head <- head(esoDS, 4)
-writeLines("Head:")
+# des Datensatzen komplett anders sein
+head <- head( esoDS, 4 )
+writeLines( "Head:" )
 head
 
-writeLines("")
+writeLines( "" )
 
-writeLines("Tail:")
-tail <- tail(esoDS, 4)
+writeLines( "Tail:" )
+tail <- tail( esoDS, 4 )
 tail
 
-writeLines("")
-writeLines("Da es sich um Gruppierungen handelt, sind Mittelwerte eher")
-writeLines("nicht nützlich.")
-writeLines("")
+writeLines( "" )
+writeLines( "Da es sich um Gruppierungen handelt, sind Mittelwerte eher" )
+writeLines( "nicht nützlich." )
+writeLines( "" )
 
-# Daher möchte ich einen Überblick über die Anzahl der Verteilungen
-# je Segment aufgeteilt nach der Gruppierung innerhalb des Segmentes
-# und der Aufteilung nach den Altersgruppen
+# Daher möchte ich einen Überblick über die Aufteilung der Datensätze nach
+# Zuordnung zu einer Alkoholgruppe in Verbidnung mit einer Altersgruppe haben.
+# Anschließend das Gleiche für Tabak und Alter.
 
-barPlotAlkohol <- barplot(table(esoDS$agegp, esoDS$alcgp),
-                          beside = TRUE,
-                          xlab = "Alkoholgenuss",
-                          ylab = "Anzahl Datensätze",
-                          main = "Verteilung Alkohol", legend = TRUE)
+barPlotAlkohol <- barplot( table( esoDS$agegp, esoDS$alcgp ),
+                           beside = TRUE,
+                           xlab = "Alkoholgenuss",
+                           ylab = "Anzahl Datensätze",
+                           main = "Verteilung Alkohol", legend = TRUE )
 
-writeLines("Übersicht Anzahl über Alkoholgenuss, Alter.")
-writeLines("Ausgabe im Fenster Plots und als Matrix auf der Konsole:")
+writeLines( "Übersicht Anzahl über Alkoholgenuss, Alter." )
+writeLines( "Ausgabe im Fenster Plots und als Matrix auf der Konsole:" )
 barPlotAlkohol
 
-barPlotTabak <- barplot(table(esoDS$agegp, esoDS$tobgp),
-                        beside = TRUE,
-                        xlab = "Tabakgenuss",
-                        ylab = "Anzahl Datensätze",
-                        main = "Verteilung Alkohol", legend = TRUE)
+barPlotTabak <- barplot( table( esoDS$agegp, esoDS$tobgp ),
+                         beside = TRUE,
+                         xlab = "Tabakgenuss",
+                         ylab = "Anzahl Datensätze",
+                         main = "Verteilung Tabak", legend = TRUE )
 
-writeLines("")
-writeLines("Übersicht Anzahl über Tabakgenuss, Alter.")
-writeLines("Ausgabe im Fenster Plots und als Matrix auf der Konsole:")
+writeLines( "" )
+writeLines( "Übersicht Anzahl über Tabakgenuss, Alter." )
+writeLines( "Ausgabe im Fenster Plots und als Matrix auf der Konsole:" )
 barPlotTabak
 
-writeLines("")
-writeLines("Abschließend das gesammte DataSet")
-writeLines("")
+writeLines( "" )
+writeLines( "Abschließend das gesammte DataSet" )
+writeLines( "" )
 
-#esoDS
+esoDS
